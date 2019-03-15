b2effc9476d0231df098a09b0d0e48f0257ddcfd
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-03-15 21:06:04 +0100

    Add docstring to tvem.util.get

--- tvem/util/_util.py
+++ tvem/util/_util.py
@@ -2,8 +2,17 @@
 # Copyright (C) 2019 Machine Learning Group of the University of Oldenburg.
 # Licensed under the Academic Free License version 3.0
 
-from typing import Dict
+from typing import Dict, Any
 
+def get(d: Dict[Any, Any], *keys: Any):
+    """Shorthand to retrieve valus at specified keys from dictionary.
 
-def get(d: Dict, *keys):
+    :param d: input dictionary
+    :param keys: a list of keys for dictionary d
+
+    Example usage::
+
+        val1, val2 = get(my_dict, 'key1', 'key2')
+    """
+
     return map(d.get, keys)
