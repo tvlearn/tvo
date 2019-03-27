0aaafd140bf2a85c96b5ccff15d28db0ce52e8c8
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-03-27 02:44:14 +0100

    [NFC] Improve tvem.device docstring

--- tvem/__init__.py
+++ tvem/__init__.py
@@ -7,7 +7,9 @@ device: _to.device = _to.device('cpu')
     Note that certain operations might run on CPU independently of
     the value of tvem.device.
 
-    To change the default, simply assign a new torch.device to tvem.device.
+    The default ('cpu') can be overridden by exporting the TVEM_USE_GPU
+    environment variable with a non-zero value. At runtime, simply assign
+    a new torch.device to tvem.device to change the framework's behavior.
 """
 if 'TVEM_USE_GPU' in _os.environ and _os.environ['TVEM_USE_GPU'] != 0:
     device = _to.device('cuda:0')
