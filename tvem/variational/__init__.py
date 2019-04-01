2b0d1634de16525103825b7aa99512ed467ce08c
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-04-01 20:44:42 +0200

    Add EEMVariationalStates to __init__.py, and therefore docs

--- tvem/variational/__init__.py
+++ tvem/variational/__init__.py
@@ -1,4 +1,5 @@
 from .TVEMVariationalStates import TVEMVariationalStates
 from .fullem import FullEM
 from .RandomSampledVarStates import RandomSampledVarStates
-__all__ = ['TVEMVariationalStates', 'FullEM', 'RandomSampledVarStates']
+from .eem import EEMVariationalStates
+__all__ = ['TVEMVariationalStates', 'FullEM', 'RandomSampledVarStates', 'EEMVariationalStates']
