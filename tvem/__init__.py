15478fdcba757bfd2b8a91232f236bbb2b5cd9c9
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-04-02 13:04:31 +0200

    Improve documentation of tvem.policy

--- tvem/__init__.py
+++ tvem/__init__.py
@@ -17,17 +17,25 @@ if 'TVEM_GPU' in _os.environ and _os.environ['TVEM_GPU'] != 0:
 
 class Policy:
     def __init__(self, policy: str):
+        """Execution policy.
+
+        :param policy: supported policies are `'seq'` (for sequential execution)
+                       and `'dist'` (for distributed execution).
+        """
         assert policy in ('seq', 'dist'), 'Supported policies are "seq" and "dist"'
         self.policy = policy
 
+    def __repr__(self):
+        return f"Policy('{self.policy}')"
+
 
 policy: Policy = Policy('seq')
-"""The preferred parallelization policy. Can be one of 'seq' or 'dist'.
+"""The preferred parallelization policy. Can be one of Policy('seq') or Poicy('dist').
 
-    - 'seq': the framework will not perform any parallelization other than
-    what torch tensors offer out of the box on the relevant device.
-    - 'dist': the framework will perform data parallelization for the algorithms
-    that implement it.
+    * 'seq': the framework will not perform any parallelization other
+      than what torch tensors offer out of the box on the relevant device.
+    * 'dist': the framework will perform data parallelization
+      for the algorithms that implement it.
 
     The default ('seq') can be overridden by exporting the TVEM_DISTRIBUTED
     environment variable with a non-zero value. At runtime, simply assign
