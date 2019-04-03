80dcbf7c176754350edb588be08ff85e64fc9463
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-04-03 21:25:08 +0200

    Refactor .gitlab-ci.yml
    - add a header with some comments
    - remove redundant instructions
    - make pytest output verbose
    - let only one MPI process print to stdout when running tests

---
+++ .ci_scripts/run_parallel_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (C) 2019 Machine Learning Group of the University of Oldenburg.
+# Licensed under the Academic Free License version 3.0
+
+# Run tests and only have one MPI process to print to stdout.
+# Run as `mpirun -n N bash run_parallel_tests.sh` where N is the desired number of processes.
+
+if [[ ! -v OMPI_COMM_WORLD_RANK ]]; then
+   echo 'Usage: mpirun -n <N> bash $0' >&2
+   exit 1
+fi
+
+if [[ $OMPI_COMM_WORLD_RANK -eq 0 ]]; then
+   pytest -v -m parallel test
+else
+   pytest -m parallel test > /dev/null
+fi
