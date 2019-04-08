afff3c0b867958c5b2e6da40a696a2a328412fac
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-04-08 18:28:13 +0200

    Rename pytest mark parallel => mpi, part 3

--- .ci_scripts/run_mpi_tests.sh
+++ .ci_scripts/run_mpi_tests.sh
@@ -3,7 +3,7 @@
 # Licensed under the Academic Free License version 3.0
 
 # Run tests and only have one MPI process to print to stdout.
-# Run as `mpirun -n N bash run_parallel_tests.sh` where N is the desired number of processes.
+# Run as `mpirun -n N bash run_mpi_tests.sh` where N is the desired number of processes.
 
 if [[ ! -v OMPI_COMM_WORLD_RANK ]]; then
    echo 'Usage: mpirun -n <N> bash $0' >&2
@@ -11,7 +11,7 @@ if [[ ! -v OMPI_COMM_WORLD_RANK ]]; then
 fi
 
 if [[ $OMPI_COMM_WORLD_RANK -eq 0 ]]; then
-   pytest --cov=tvem --cov-append -v -m parallel test
+   pytest --cov=tvem --cov-append -v -m mpi test
 else
-   pytest -m parallel test > /dev/null
+   pytest -m mpi test > /dev/null
 fi
