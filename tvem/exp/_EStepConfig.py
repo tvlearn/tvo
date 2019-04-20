c9b0933ed803278ba5a835b9485c8d301f43839f
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-04-20 21:28:36 +0200

    Add helper classes EStepConfig, EEMConfig
    The abstract base class EStepConfig represents a generic E-step
    configuration, EEMConfig represents configuration for EEM.

---
+++ tvem/exp/_EStepConfig.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 Machine Learning Group of the University of Oldenburg.
+# Licensed under the Academic Free License version 3.0
+
+from abc import ABC
+
+
+class EStepConfig(ABC):
+    def __init__(self, n_states: int):
+        """Abstract base configuration object for experiments' E-steps.
+
+        :param n_states: Number of variational states per datapoint to keep in memory.
+        """
+        self.n_states = n_states
+
+
+class EEMConfig(EStepConfig):
+    def __init__(self, n_states: int, n_parents: int, n_children: int,
+                 n_generations: int, parent_selection: str = 'fitness', crossover: bool = True,
+                 mutation: str = 'uniform'):
+        """Configuration object for EEM E-step.
+
+        :param n_states: Number of variational states per datapoint to keep in memory.
+        :param n_parents: Number of parent states to select at each EEM generation.
+                          Must be <= n_states.
+        :param n_children: Number of children per parent to generate via mutation
+                           at each EEM generation.
+        :param parent_selection: Parent selection algorithm for EEM. Must be one of:
+                                 - 'fitness': fitness-proportional parent selection
+                                 - 'uniform': random uniform parent selection
+        :param crossover: Whether crossover should be applied or not.
+        :param mutation: Mutation algorithm for EEM. Must be one of:
+                         - 'sparsity': bits are flipped so that states tend
+                            towards current model sparsity.
+                         - 'uniform': random uniform selection of bits to flip.
+        """
+        valid_selections = ('fitness', 'random')
+        assert parent_selection in valid_selections, f'Unknown parent selection {parent_selection}'
+        valid_mutations = ('sparsity', 'uniform')
+        assert mutation in valid_mutations, f'Unknown mutation {mutation}'
+        assert n_parents <= n_states,\
+            f'n_parents ({n_parents}) must be lower than n_states ({n_states})'
+
+        self.n_parents = n_parents
+        self.n_children = n_children
+        self.n_generations = n_generations
+        self.parent_selection = parent_selection
+        self.crossover = crossover
+        self.mutation = mutation
+
+        super().__init__(n_states)
