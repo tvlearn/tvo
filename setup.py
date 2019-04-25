bb1f04826bceb38b0aa3e83a526462bdcb59d784
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-04-25 16:13:15 +0200

    Remove version from setup.py, we already set it in meta.yaml

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="tvem", version="0.1", packages=find_packages(exclude=("test",)), zip_safe=False)
+setup(name="tvem", packages=find_packages(exclude=("test",)), zip_safe=False)
