a72b63608fc89dd6553d6bde606088029b2ad315
Jakob Drefs <drefs.jakob@gmail.com>
2019-05-28 17:57:44 +0200

    Change import of ExpConfig in exp/__init__.py.

--- tvem/exp/__init__.py
+++ tvem/exp/__init__.py
@@ -1,4 +1,5 @@
-from ._experiments import Training, Testing, ExpConfig
+from ._experiments import Training, Testing
 from ._EStepConfig import EEMConfig, FullEMConfig
+from ._ExpConfig import ExpConfig
 
 __all__ = ["Training", "Testing", "EEMConfig", "FullEMConfig", "ExpConfig"]
