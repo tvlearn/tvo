edf8c3e84cb42fc905790fceed9b6fa8f7255efc
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-06-07 11:17:09 +0200

    Add test for ShufflingSampler

--- test/dataloader_test.py
+++ test/dataloader_test.py
@@ -3,10 +3,12 @@
 # Licensed under the Academic Free License version 3.0
 
 import torch as to
+import torch.distributed as dist
 import pytest
 from munch import Munch
 
-from tvem.utils.data import TVEMDataLoader
+from tvem.utils.data import TVEMDataLoader, ShufflingSampler
+from tvem.utils.parallel import init_processes
 import tvem
 
 
@@ -29,3 +31,20 @@ def test_two_datasets(setup):
     assert DataLoader.dataset.tensors[0].equal(to.arange(setup.N))
     assert to.allclose(DataLoader.dataset.tensors[1], setup.set1)
     assert to.allclose(DataLoader.dataset.tensors[2], setup.set2)
+
+
+@pytest.mark.mpi
+def test_shuffling_sampler(setup):
+    if tvem.get_run_policy() == "mpi":
+        init_processes()
+    n_procs = dist.get_world_size() if tvem.get_run_policy() == "mpi" else 1
+
+    data = setup.set1
+    n_samples = (setup.N + n_procs - 1) // n_procs
+    sampler = ShufflingSampler(data, n_samples)
+    dl = TVEMDataLoader(data, batch_size=setup.batch_size, sampler=sampler)
+    n_entries_per_proc = 0
+    for idx, batch in dl:
+        n_entries_per_proc += idx.numel()
+    # check all processes looped over n_samples datapoints, independently of the dataset size
+    assert n_entries_per_proc == n_samples
