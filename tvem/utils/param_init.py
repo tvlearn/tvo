d31818879f54f31a9e4100286c749a775c00b745
Jakob Drefs <drefs.jakob@gmail.com>
2019-09-19 17:00:00 +0200

    [NFC] Fix format in docs of `param_init.py`

--- tvem/utils/param_init.py
+++ tvem/utils/param_init.py
@@ -12,12 +12,12 @@ def init_W_data_mean(
 ) -> Tensor:
     """Initialize weights W based on noisy mean of the data points.
 
-    param data_mean: Mean of all data points. Length equals data dimensionality D.
-    param data_var: Variance of all data points in each dimension d=1,...D.
-    param H: Number of basis functions to be generated.
-    param dtype: dtype of output Tensor. Defaults to torch.float64.
-    param device: torch.device of output Tensor. Defaults to tvem.get_device().
-    returns: Weight matrix W with shape (D,H).
+    :param data_mean: Mean of all data points. Length equals data dimensionality D.
+    :param data_var: Variance of all data points in each dimension d=1,...D.
+    :param H: Number of basis functions to be generated.
+    :param dtype: dtype of output Tensor. Defaults to torch.float64.
+    :param device: torch.device of output Tensor. Defaults to tvem.get_device().
+    :returns: Weight matrix W with shape (D,H).
     """
 
     if device is None:
@@ -32,10 +32,10 @@ def init_sigma_default(
 ) -> Tensor:
     """Initialize scalar sigma parameter based on variance of the data points.
 
-    param data_var: Variance of all data points in each dimension d=1,...D of the data.
-    param dtype: dtype of output Tensor. Defaults to torch.float64.
-    param device: torch.device of output Tensor. Defaults to tvem.get_device().
-    returns: Scalar sigma parameter.
+    :param data_var: Variance of all data points in each dimension d=1,...D of the data.
+    :param dtype: dtype of output Tensor. Defaults to torch.float64.
+    :param device: torch.device of output Tensor. Defaults to tvem.get_device().
+    :returns: Scalar sigma parameter.
 
     Returns the mean of the variance in each dimension d=1,...,D.
     """
@@ -50,11 +50,11 @@ def init_pies_default(
 ):
     """Initialize pi parameter based on given crowdedness.
 
-    param H: Length of pi vector.
-    param crowdedness: Average crowdedness corresponding to sum of elements in vector pi.
-    param dtype: dtype of output Tensor. Defaults to torch.float64.
-    param device: torch.device of output Tensor. Defaults to tvem.get_device().
-    returns: Vector pi.
+    :param H: Length of pi vector.
+    :param crowdedness: Average crowdedness corresponding to sum of elements in vector pi.
+    :param dtype: dtype of output Tensor. Defaults to torch.float64.
+    :param device: torch.device of output Tensor. Defaults to tvem.get_device().
+    :returns: Vector pi.
     """
 
     if device is None:
