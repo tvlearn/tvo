0b2e81e9df0ea6fde160be7703628113468158c0
Jakob Drefs <drefs.jakob@gmail.com>
2019-10-09 15:09:03 +0200

    Change utils.__init__ after 7961ed0

--- tvem/utils/__init__.py
+++ tvem/utils/__init__.py
@@ -1,5 +1,6 @@
 from ._utils import get
 from .H5Logger import H5Logger
 from .CyclicLR import CyclicLR
+from .gen import genBars
 
-__all__ = ["get", "H5Logger", "CyclicLR"]
+__all__ = ["get", "H5Logger", "CyclicLR", "genBars"]
