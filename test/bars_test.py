e18ab484be2e29d45d4d8e771d90f933087806e9
Jakob Drefs <drefs.jakob@gmail.com>
2019-10-09 15:09:32 +0200

    [TEST] Adapt bars_test.py according to 7961ed0

--- test/bars_test.py
+++ test/bars_test.py
@@ -6,7 +6,7 @@
 from tvem.exp import ExpConfig, FullEMConfig, Training
 from tvem.models import NoisyOR, BSC
 from tvem.utils.parallel import init_processes, broadcast
-from tvem.utils import get
+from tvem.utils import get, genBars
 import tvem
 import os
 import numpy as np
@@ -17,47 +17,6 @@ import torch.distributed as dist
 from munch import Munch
 
 gpu_and_mpi_marks = pytest.param(tvem.get_device().type, marks=(pytest.mark.gpu, pytest.mark.mpi))
-
-
-def generate_bars(
-    H: int,
-    bar_amp: float = 1.0,
-    neg_amp: bool = False,
-    bg_amp: float = 0.0,
-    add_unit: float = None,
-    precision: to.dtype = to.float64,
-):
-    """ Generate a ground-truth dictionary W suitable for a std. bars test
-
-    Creates H bases vectors with horizontal and vertival bars on a R*R pixel grid,
-    (wth R = H // 2).
-
-    :param H: Number of latent variables
-    :param bar_amp: Amplitude of each bar
-    :param neg_amp: Set probability of amplitudes taking negative values to 50 percent
-    :param bg_amp: Background amplitude
-    :param add_unit: If not None an additional unit with amplitude add_unit will be inserted
-    :param precision: torch.dtype of the returned tensor
-    :returns: tensor containing the bars dictionary
-    """
-    R = H // 2
-    D = R ** 2
-
-    W = bg_amp * to.ones((R, R, H), dtype=precision, device=tvem.get_device())
-    for i in range(R):
-        W[i, :, i] = bar_amp
-        W[:, i, R + i] = bar_amp
-
-    if neg_amp:
-        sign = 1 - 2 * to.randint(high=2, size=(H), device=tvem.get_device())
-        W = sign[None, None, :] * W
-
-    if add_unit is not None:
-        add_unit = add_unit * to.ones((D, 1), device=tvem.get_device())
-        W = to.cat((W, add_unit), dim=1)
-        H += 1
-
-    return W.view((D, H))
 
 
 @pytest.fixture(scope="module", params=(gpu_and_mpi_marks,))
@@ -101,7 +60,7 @@ def model_and_data(request, hyperparams, estep_conf):
 
     if request.param == "BSC":
 
-        W_gt = generate_bars(H, bar_amp=10.0, precision=precision)
+        W_gt = genBars(H, bar_amp=10.0, precision=precision)
         sigma_gt = to.ones((1,), dtype=precision, device=tvem.get_device())
         pies_gt = to.full((H,), 2.0 / H, dtype=precision, device=tvem.get_device())
 
@@ -127,7 +86,7 @@ def model_and_data(request, hyperparams, estep_conf):
 
     elif request.param == "NoisyOR":
 
-        W_gt = generate_bars(H, bar_amp=0.8, bg_amp=0.1, precision=precision)
+        W_gt = genBars(H, bar_amp=0.8, bg_amp=0.1, precision=precision)
         pies_gt = to.full((H,), 2.0 / H, dtype=precision, device=tvem.get_device())
 
         to.manual_seed(999)
