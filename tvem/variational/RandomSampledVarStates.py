52695b0c1f14571b96967db2829f19c0563b0819
Jakob Drefs <drefs.jakob@gmail.com>
2019-11-26 17:23:30 +0100

    [RandomSampledVarStates] Remove `n_new_states` argument in `RandomSampledVarStates.__init__` and use `config[S_new]` instead.

--- tvem/variational/RandomSampledVarStates.py
+++ tvem/variational/RandomSampledVarStates.py
@@ -15,16 +15,14 @@ if TYPE_CHECKING:
 
 
 class RandomSampledVarStates(TVEMVariationalStates):
-    def __init__(self, n_new_states: int, conf: Dict[str, Any], sparsity: float = 0.5):
+    def __init__(self, conf: Dict[str, Any], sparsity: float = 0.5):
         """A TVEMVariationalStates implementation that performs random sampling.
 
-        :param n_new_states: number of states to be sampled at every call to ~update
         :param conf: dictionary with hyper-parameters. See\
           :func:`~tvem.variational.TVEMVariationalStates` for a list of required keys.
         :param sparsity: average fraction of active units in sampled states.
         """
         super().__init__(conf)
-        self.n_new_states = n_new_states
         self.sparsity = sparsity
 
     def update(self, idx: Tensor, batch: Tensor, model: "TVEMModel") -> int:
@@ -37,7 +35,7 @@ class RandomSampledVarStates(TVEMVariationalStates):
         K = self.K[idx]
         batch_size, S, H = K.shape
         self.lpj[idx] = lpj_fn(batch, K)
-        new_K = (to.rand(batch_size, self.n_new_states, H, device=K.device) < self.sparsity).byte()
+        new_K = (to.rand(batch_size, self.config["S_new"], H, device=K.device) < self.sparsity).byte()
         new_lpj = lpj_fn(batch, new_K)
 
         return update_states_for_batch(
