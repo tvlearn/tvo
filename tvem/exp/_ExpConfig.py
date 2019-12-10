36d448f38de95741deb1b4d248523c119d555301
Marius Walz <faer3014@lenny.w2kroot.uni-oldenburg.de>
2019-12-10 14:08:58 +0100

    Readded necessary import

--- tvem/exp/_ExpConfig.py
+++ tvem/exp/_ExpConfig.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019 Machine Learning Group of the University of Oldenburg.
 # Licensed under the Academic Free License version 3.0
+import os
 
 from typing import Iterable, Sequence, Dict, Any
-
 
 class ExpConfig:
     def __init__(
