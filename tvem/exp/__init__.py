2bfb9fe55468c5695d3c8ee6dafb03a3482f6db1
Jakob Drefs <drefs.jakob@gmail.com>
2021-04-08 01:06:37 +0200

    [TVS] Include TVSConfig in exp/__init__.py

--- tvem/exp/__init__.py
+++ tvem/exp/__init__.py
@@ -1,6 +1,6 @@
 from ._experiments import Training, Testing
-from ._EStepConfig import EEMConfig, FullEMConfig
+from ._EStepConfig import EEMConfig, TVSConfig, FullEMConfig
 from ._ExpConfig import ExpConfig
 from ._EpochLog import EpochLog
 
-__all__ = ["Training", "Testing", "EEMConfig", "FullEMConfig", "ExpConfig", "EpochLog"]
+__all__ = ["Training", "Testing", "EEMConfig", "TVSConfig", "FullEMConfig", "ExpConfig", "EpochLog"]
