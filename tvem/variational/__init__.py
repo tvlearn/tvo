0716a4d5450fbd0b61d4458b1df1663eabdfe395
Jakob Drefs <drefs.jakob@gmail.com>
2021-04-08 01:05:12 +0200

    [TVS] Include TVSVariationalStates in variational/__init__.py

--- tvem/variational/__init__.py
+++ tvem/variational/__init__.py
@@ -2,5 +2,12 @@ from .TVEMVariationalStates import TVEMVariationalStates
 from .fullem import FullEM
 from .RandomSampledVarStates import RandomSampledVarStates
 from .eem import EEMVariationalStates
+from .tvs import TVSVariationalStates
 
-__all__ = ["TVEMVariationalStates", "FullEM", "RandomSampledVarStates", "EEMVariationalStates"]
+__all__ = [
+    "TVEMVariationalStates",
+    "FullEM",
+    "RandomSampledVarStates",
+    "EEMVariationalStates",
+    "TVSVariationalStates",
+]
