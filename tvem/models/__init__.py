5878d2de0309a7d8c79e9d87a805c85e3c742cbd
L_Weg <lennart.wegmann97@googlemail.com>
2021-11-08 16:05:31 +0100

    added multivatiate gmm (sing. with bars_test)

--- tvem/models/__init__.py
+++ tvem/models/__init__.py
@@ -4,5 +4,6 @@ from .tvae import TVAE
 from .gmm import GMM
 from .rem_gmm import REM1_GMM
 from .pmm import PMM
+from .full_gmm import FULL_GMM
 
 __all__ = ["NoisyOR", "BSC", "TVAE", "REM1_GMM", "GMM", "PMM"]
