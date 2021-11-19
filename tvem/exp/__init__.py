8bfd81e65d85f27530279bdb0b5fb775ce38de60
Jakob Drefs <drefs.jakob@gmail.com>
2021-11-19 14:07:22 +0100

    [RandomSampling] Add RandomSamplingConfig to exp/__init__.py

--- tvem/exp/__init__.py
+++ tvem/exp/__init__.py
@@ -1,5 +1,11 @@
 from ._experiments import Training, Testing
-from ._EStepConfig import EEMConfig, TVSConfig, FullEMConfig, FullEMSingleCauseConfig
+from ._EStepConfig import (
+    EEMConfig,
+    TVSConfig,
+    RandomSamplingConfig,
+    FullEMConfig,
+    FullEMSingleCauseConfig,
+)
 from ._ExpConfig import ExpConfig
 from ._EpochLog import EpochLog
 
@@ -10,6 +16,7 @@ __all__ = [
     "FullEMConfig",
     "TVSConfig",
     "FullEMSingleCauseConfig",
+    "RandomSamplingConfig",
     "ExpConfig",
     "EpochLog",
 ]
