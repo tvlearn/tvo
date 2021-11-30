d9b1da8b433b5d300f5339e2c5c0aa5640093e70
Filippos Panagiotou <filippos.panagiotou@outlook.com>
2021-11-30 21:13:50 +0100

    rebased hyperoptimization

---
+++ hyperoptimization/__init__.py
@@ -0,0 +1,6 @@
+from .models import FCDeConvNetSigOut, FCDeConvNet
+from .workers import TVAEWorker
+from .explore import print_best
+from .runs import local_sequential
+
+__all__ = ["FCDeConvNet", "FCDeConvNetSigOut", "TVAEWorker", "print_best", "local_sequential"]
