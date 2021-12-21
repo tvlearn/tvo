dcdef32166462884b79f0e4876ba134855a2f6b2
Jakob Drefs <drefs.jakob@gmail.com>
2021-12-21 01:39:06 +0100

    [INIT] Let init_W_data_mean and init_sigma_default take data set as input argument

--- tvem/utils/param_init.py
+++ tvem/utils/param_init.py
@@ -1,13 +1,13 @@
 import tvem
+import numpy as np
 import torch as to
 from torch import Tensor
 
 
 def init_W_data_mean(
-    data_mean: Tensor,
-    data_var: Tensor,
+    data: Tensor,
     H: int,
-    std_factor: float = 0.01,
+    std_factor: float = 0.25,
     dtype: to.dtype = to.float64,
     device: to.device = None,
 ) -> Tensor:
@@ -21,16 +21,16 @@ def init_W_data_mean(
     :param device: torch.device of output Tensor. Defaults to tvem.get_device().
     :returns: Weight matrix W with shape (D,H).
     """
-
-    if device is None:
-        device = tvem.get_device()
-    return data_mean.to(dtype=dtype, device=device).repeat((H, 1)).t() + std_factor * to.mean(
-        to.sqrt(data_var.to(dtype=dtype, device=device))
-    ) * to.randn((len(data_mean), H), dtype=dtype, device=device)
+    device_ = tvem.get_device() if device is None else device
+    data_nanmean = to.from_numpy(np.nanmean(data, axis=0)).to(dtype=dtype, device=device_)
+    var = init_sigma_default(data, dtype, device_)
+    return data_nanmean.repeat((H, 1)).t() + std_factor * to.sqrt(var) * to.randn(
+        (len(data_nanmean), H), dtype=dtype, device=device_
+    )
 
 
 def init_sigma_default(
-    data_var: Tensor, dtype: to.dtype = to.float64, device: to.device = None
+    data: Tensor, dtype: to.dtype = to.float64, device: to.device = None
 ) -> Tensor:
     """Initialize scalar sigma parameter based on variance of the data points.
 
@@ -41,10 +41,9 @@ def init_sigma_default(
 
     Returns the mean of the variance in each dimension d=1,...,D.
     """
-
-    if device is None:
-        device = tvem.get_device()
-    return to.mean(to.sqrt(data_var.to(dtype=dtype, device=device)), dim=0, keepdim=True)
+    _device = tvem.get_device() if device is None else device
+    cov = to.from_numpy(np.cov(data.numpy().T)).to(device=_device, dtype=dtype)
+    return to.mean(to.diag(cov)) + to.tensor([0.001], device=_device, dtype=dtype)
 
 
 def init_pies_default(
