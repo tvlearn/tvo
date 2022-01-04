[TVAE] Fix gradient estimation
Before this patch, truncated posteriors were not considered constant
for the purposes of the evaluation of the free energy gradients, i.e.
the M-step was optimizing the parameters _and_ the truncated posteriors.

With this patch, we use a copy of the log-pseudo-joints that is detached
from the gradient computation in order to compute the mean over
posteriors of the log-joints, which results in a computation that
effectively considers truncated posteriors constant for the purposes
of the optimization of parameters in the M-step.

As a consequence TVAE is not able to evaluate partial values of the
free energy for every batch, so some code is updated to have
TVAE.update_update_param_batch return None instead of the free energy
value.

tvem/models/tvae.py
@@ -117,13 +117,13 @@ class _TVAE(Trainable, Sampler, Reconstructor):
 
     def update_param_batch(
         self, idx: to.Tensor, batch: to.Tensor, states: TVEMVariationalStates
-    ) -> float:
+    ) -> None:
         if to.isnan(batch).any():
             raise RuntimeError("There are NaNs in this batch")
-        F, mlp_out = self._optimize_nn_params(idx, batch, states)
+        mlp_out = self._optimize_nn_params(idx, batch, states)
         with to.no_grad():
             self._accumulate_param_updates(idx, batch, states, mlp_out)
-        return F
+        return None
 
     @property
     def shape(self) -> Tuple[int, ...]:
@@ -140,7 +140,7 @@ class _TVAE(Trainable, Sampler, Reconstructor):
 
     def _optimize_nn_params(
         self, idx: to.Tensor, data: to.Tensor, states: TVEMVariationalStates
-    ) -> Tuple[float, to.Tensor]:
+    ) -> to.Tensor:
         """
         Gradient-based optimized parameters are changed in-place. All other arguments are left
         untouched.
@@ -150,8 +150,9 @@ class _TVAE(Trainable, Sampler, Reconstructor):
         assert self._optimizer is not None  # to make mypy happy
 
         lpj, mlp_out = self._lpj_and_mlpout(data, states.K[idx])
-        F = self._free_energy_from_logjoints(self.log_joint(data, states.K[idx], lpj))
-        loss = -F / data.shape[0]
+        lpj_no_grad = lpj.detach()
+        objective = mean_posterior(self.log_joint(data, states.K[idx], lpj), lpj_no_grad).sum(dim=0)
+        loss = -objective / data.shape[0]
         loss.backward()
 
         mpi_average_grads(self.theta)
@@ -159,7 +160,7 @@ class _TVAE(Trainable, Sampler, Reconstructor):
         self._scheduler.step()
         self._optimizer.zero_grad()
 
-        return F.item(), mlp_out
+        return mlp_out
 
     def _accumulate_param_updates(
         self, idx: to.Tensor, data: to.Tensor, states: TVEMVariationalStates, mlp_out: to.Tensor
@@ -409,8 +410,8 @@ class GaussianTVAE(_TVAE):
 
     def _optimize_nn_params(
         self, idx: to.Tensor, data: to.Tensor, states: TVEMVariationalStates
-    ) -> Tuple[float, to.Tensor]:
-        F, mlp_out = super()._optimize_nn_params(idx, data, states)
+    ) -> to.Tensor:
+        mlp_out = super()._optimize_nn_params(idx, data, states)
 
         with to.no_grad():
             sigma2 = self.theta["sigma2"]
@@ -420,7 +421,7 @@ class GaussianTVAE(_TVAE):
             if pi.requires_grad:
                 to.clamp(pi, 1e-5, 1 - 1e-5, out=pi)
 
-        return F, mlp_out
+        return mlp_out
 
     def _accumulate_param_updates(
         self, idx: to.Tensor, data: to.Tensor, states: TVEMVariationalStates, mlp_out: to.Tensor
@@ -656,15 +657,15 @@ class BernoulliTVAE(_TVAE):
 
     def _optimize_nn_params(
         self, idx: to.Tensor, data: to.Tensor, states: TVEMVariationalStates
-    ) -> Tuple[float, to.Tensor]:
-        F, mlp_out = super()._optimize_nn_params(idx, data, states)
+    ) -> to.Tensor:
+        mlp_out = super()._optimize_nn_params(idx, data, states)
 
         with to.no_grad():
             pi = self.theta["pies"]
             if pi.requires_grad:
                 to.clamp(pi, 1e-5, 1 - 1e-5, out=pi)
 
-        return F, mlp_out
+        return mlp_out
 
     def _accumulate_param_updates(
         self, idx: to.Tensor, data: to.Tensor, states: TVEMVariationalStates, mlp_out: to.Tensor