8c33e17b98848c4b684aa5665eee51ec5361d8ef
Jakob Drefs <drefs.jakob@gmail.com>
2022-01-05 10:57:57 +0100

    [INIT] Rename init_sigma_default->init_sigma2_default. Use np.nanvar in init_sigma2_default.

--- tvem/utils/param_init.py
+++ tvem/utils/param_init.py
@@ -13,8 +13,7 @@ def init_W_data_mean(
 ) -> Tensor:
     """Initialize weights W based on noisy mean of the data points.
 
-    :param data_mean: Mean of all data points. Length equals data dimensionality D.
-    :param data_var: Variance of all data points in each dimension d=1,...D.
+    :param data: Data set, is (N, D).
     :param H: Number of basis functions to be generated.
     :param std_factor: Scalar to control amount of standard deviation of additive noise
     :param dtype: dtype of output Tensor. Defaults to torch.float64.
@@ -25,18 +24,18 @@ def init_W_data_mean(
     data_nanmean = to.from_numpy(np.nanmean(data.detach().cpu().numpy(), axis=0)).to(
         dtype=dtype, device=device_
     )
-    var = init_sigma_default(data, dtype, device_)
+    var = init_sigma2_default(data, dtype, device_)
     return data_nanmean.repeat((H, 1)).t() + std_factor * to.sqrt(var) * to.randn(
         (len(data_nanmean), H), dtype=dtype, device=device_
     )
 
 
-def init_sigma_default(
+def init_sigma2_default(
     data: Tensor, dtype: to.dtype = to.float64, device: to.device = None
 ) -> Tensor:
     """Initialize scalar sigma parameter based on variance of the data points.
 
-    :param data_var: Variance of all data points in each dimension d=1,...D of the data.
+    :param data: Data set, is (N, D).
     :param dtype: dtype of output Tensor. Defaults to torch.float64.
     :param device: torch.device of output Tensor. Defaults to tvem.get_device().
     :returns: Scalar sigma parameter.
@@ -44,8 +43,10 @@ def init_sigma_default(
     Returns the mean of the variance in each dimension d=1,...,D.
     """
     _device = tvem.get_device() if device is None else device
-    cov = to.from_numpy(np.cov(data.detach().cpu().numpy().T)).to(device=_device, dtype=dtype)
-    return to.mean(to.diag(cov)) + to.tensor([0.001], device=_device, dtype=dtype)
+    var = to.from_numpy(np.nanvar(data.detach().cpu().numpy(), axis=0)).to(
+        device=_device, dtype=dtype
+    )
+    return to.mean(var) + to.tensor([0.001], device=_device, dtype=dtype)
 
 
 def init_pies_default(
