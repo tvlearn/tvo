83683a641aef082bdadcc8082994322756ed2183
Filippos <panagiotou.filippos@outlook.com>
2022-04-28 12:21:26 +0200

    restoring TVAE

--- tvo/models/__init__.py
+++ tvo/models/__init__.py
@@ -1,7 +1,8 @@
 from .noisyor import NoisyOR
 from .bsc import BSC
+from .tvae import GaussianTVAE, BernoulliTVAE
 from .sssc import SSSC
 from .gmm import GMM
 from .pmm import PMM
 
-__all__ = ["NoisyOR", "BSC", "SSSC", "GMM", "PMM"]
+__all__ = ["NoisyOR", "BSC", "GaussianTVAE", "BernoulliTVAE", "GMM", "PMM"]
