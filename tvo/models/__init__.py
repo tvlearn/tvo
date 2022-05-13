95b5d60eed21d47015e9229c37c4568ead37bfd1
Hamid Mousavi <hamidmousavi92@yahoo.com>
2022-05-13 15:34:36 +0200

    embedded the pmca model into the init file

--- tvo/models/__init__.py
+++ tvo/models/__init__.py
@@ -3,5 +3,6 @@ from .bsc import BSC
 from .sssc import SSSC
 from .gmm import GMM
 from .pmm import PMM
+from .pmca import PMCA
 
-__all__ = ["NoisyOR", "BSC", "SSSC", "GMM", "PMM"]
+__all__ = ["NoisyOR", "BSC", "SSSC", "GMM", "PMM", "PMCA"]
