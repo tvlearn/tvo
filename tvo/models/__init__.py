068f6bf095f09d5fd8907306b2e9a6c551cc4f39
Jakob Drefs <52153848+jdrefs@users.noreply.github.com>
2022-07-01 17:47:02 +0200

    Revert "black pass" (#27)

--- tvo/models/__init__.py
+++ tvo/models/__init__.py
@@ -1,7 +1,8 @@
 from .noisyor import NoisyOR
 from .bsc import BSC
 from .sssc import SSSC
+from .tvae import GaussianTVAE, BernoulliTVAE
 from .gmm import GMM
 from .pmm import PMM
 
-__all__ = ["NoisyOR", "BSC", "SSSC", "GMM", "PMM"]
+__all__ = ["NoisyOR", "BSC", "SSSC", "GaussianTVAE", "BernoulliTVAE", "GMM", "PMM"]
