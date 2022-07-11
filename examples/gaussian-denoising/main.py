bd120e1a24c9d5ab4def3562ec80dea4bb54aeb2
Filippos <panagiotou.filippos@outlook.com>
2022-07-11 19:37:53 +0200

    ignore complexity of example demonstration

--- examples/gaussian-denoising/main.py
+++ examples/gaussian-denoising/main.py
@@ -38,7 +38,7 @@ PRECISION = to.float32
 dtype_device_kwargs = {"dtype": PRECISION, "device": DEVICE}
 
 
-def gaussian_denoising_example():
+def gaussian_denoising_example():# noqa: C901
 
     # initialize MPI (if executed with env TVO_MPI=...), otherwise pass
     comm_rank = init_processes()[0]
