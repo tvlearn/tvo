Added gradient bug fix to new branch

tvo/models/tvae.py
@@ -37,7 +37,9 @@ def _init_W(
     if init is None:
         n_layers = len(net_shape) - 1
         W_shapes = ((net_shape[ln], net_shape[ln + 1]) for ln in range(n_layers))
-        W = list(map(to.nn.init.xavier_normal_, (to.empty(s) for s in W_shapes)))
+        W = list(
+            map(to.nn.init.xavier_normal_, (to.empty(s) for s in W_shapes))
+        )  
     else:
         assert (
             len(init) == len(net_shape) - 1
@@ -116,14 +118,17 @@ class _TVAE(Trainable, Sampler, Reconstructor):
         return Fn.sum()
 
     def update_param_batch(
-        self, idx: to.Tensor, batch: to.Tensor, states: TVOVariationalStates
-    ) -> float:
+        self,
+        idx: to.Tensor,
+        batch: to.Tensor,
+        states: TVOVariationalStates
+    ) -> None: 
         if to.isnan(batch).any():
             raise RuntimeError("There are NaNs in this batch")
-        F, mlp_out = self._optimize_nn_params(idx, batch, states)
+        mlp_out = self._optimize_nn_params(idx, batch, states)
         with to.no_grad():
             self._accumulate_param_updates(idx, batch, states, mlp_out)
-        return F
+        return None  
 
     @property
     def shape(self) -> Tuple[int, ...]:
@@ -139,8 +144,11 @@ class _TVAE(Trainable, Sampler, Reconstructor):
         return tuple(reversed(self._net_shape))
 
     def _optimize_nn_params(
-        self, idx: to.Tensor, data: to.Tensor, states: TVOVariationalStates
-    ) -> Tuple[float, to.Tensor]:
+        self,
+        idx: to.Tensor,
+        data: to.Tensor,
+        states: TVOVariationalStates
+    ) -> to.Tensor:
         """
         Gradient-based optimized parameters are changed in-place. All other arguments are left
         untouched.
@@ -150,8 +158,11 @@ class _TVAE(Trainable, Sampler, Reconstructor):
         assert self._optimizer is not None  # to make mypy happy
 
         lpj, mlp_out = self._lpj_and_mlpout(data, states.K[idx])
-        F = self._free_energy_from_logjoints(self.log_joint(data, states.K[idx], lpj))
-        loss = -F / data.shape[0]
+        lpj_no_grad = lpj.detach()  
+        objective = mean_posterior(self.log_joint(data, states.K[idx], lpj), lpj_no_grad).sum(
+            dim=0
+        )  
+        loss = -objective / data.shape[0]
         loss.backward()
 
         mpi_average_grads(self.theta)
@@ -159,7 +170,7 @@ class _TVAE(Trainable, Sampler, Reconstructor):
         self._scheduler.step()
         self._optimizer.zero_grad()
 
-        return F.item(), mlp_out
+        return mlp_out 
 
     def _accumulate_param_updates(
         self, idx: to.Tensor, data: to.Tensor, states: TVOVariationalStates, mlp_out: to.Tensor
@@ -247,7 +258,7 @@ class GaussianTVAE(_TVAE):
                 and external_model is None
             )
             or (shape is None and W_init is None and b_init is None and external_model is not None)
-        ), "Must exclusively specify one one `shape`, (`W_init`, `b_init`), `external_model`"
+        ), "Must exclusively specify one one `shape`, (`W_init`, `b_init`), `external_model`"  # either specify shape or (W, b) or external
 
         if external_model is not None:
             assert hasattr(
@@ -408,9 +419,12 @@ class GaussianTVAE(_TVAE):
         return (Y, hidden_state) if must_return_hidden_state else Y
 
     def _optimize_nn_params(
-        self, idx: to.Tensor, data: to.Tensor, states: TVOVariationalStates
-    ) -> Tuple[float, to.Tensor]:
-        F, mlp_out = super()._optimize_nn_params(idx, data, states)
+        self,
+        idx: to.Tensor,
+        data: to.Tensor,
+        states: TVOVariationalStates
+    ) -> to.Tensor:  
+        mlp_out = super()._optimize_nn_params(idx, data, states)  
 
         with to.no_grad():
             sigma2 = self.theta["sigma2"]
@@ -420,7 +434,7 @@ class GaussianTVAE(_TVAE):
             if pi.requires_grad:
                 to.clamp(pi, 1e-5, 1 - 1e-5, out=pi)
 
-        return F, mlp_out
+        return mlp_out  
 
     def _accumulate_param_updates(
         self, idx: to.Tensor, data: to.Tensor, states: TVOVariationalStates, mlp_out: to.Tensor
@@ -655,16 +669,18 @@ class BernoulliTVAE(_TVAE):
         return (Y, hidden_state) if must_return_hidden_state else Y
 
     def _optimize_nn_params(
-        self, idx: to.Tensor, data: to.Tensor, states: TVOVariationalStates
-    ) -> Tuple[float, to.Tensor]:
-        F, mlp_out = super()._optimize_nn_params(idx, data, states)
-
+        self,
+        idx: to.Tensor,
+        data: to.Tensor,
+        states: TVOVariationalStates
+    ) -> to.Tensor:  
+        mlp_out = super()._optimize_nn_params(idx, data, states)
         with to.no_grad():
             pi = self.theta["pies"]
             if pi.requires_grad:
                 to.clamp(pi, 1e-5, 1 - 1e-5, out=pi)
 
-        return F, mlp_out
+        return mlp_out
 
     def _accumulate_param_updates(
         self, idx: to.Tensor, data: to.Tensor, states: TVOVariationalStates, mlp_out: to.Tensor