Fix more numerical instabilities

examples/amortization/amortize.py
@@ -32,7 +32,11 @@ from utils.plotting import plot_epoch_log
 from utils.utils import stdout_logger
 
 
-torch.autograd.set_detect_anomaly(True)
+if False:
+    print("------------------------------------------------")
+    print("WARNING! torch.autograd.set_detect_anomaly(True)")
+    print("------------------------------------------------")
+    torch.autograd.set_detect_anomaly(True)
 
 
 if __name__ == "__main__":
@@ -45,11 +49,12 @@ if __name__ == "__main__":
     arg_parser.add_argument("--epochs_full", type=int, help="Number of epochs to train full model", default=10)
     arg_parser.add_argument("--batch_size", type=int, help="Training batch size", default=32)
     arg_parser.add_argument("--N_IS", type=int, help="Number of samples for importance sampler estimator", default=128)
-    arg_parser.add_argument("--lr", type=float, help="Learning rate", default=1e-2)
+    arg_parser.add_argument("--lr_mean", type=float, help="Learning rate for mean optimization", default=1e-2)
+    arg_parser.add_argument("--lr_full", type=float, help="Learning rate for full optimization", default=1e-2)
     arg_parser.add_argument("--t_start", type=float, help="Learning start temperature", default=1.0)
     arg_parser.add_argument("--t_end", type=float, help="Learning end temperature", default=1.0)
     arg_parser.add_argument("--CPU", help="Force CPU compute", action="store_true")
-    arg_parser.add_argument("--precision", type=FloatPrecision, help="Compute precision", default=FloatPrecision.float64)
+    arg_parser.add_argument("--precision", type=FloatPrecision, help="Compute precision", default=FloatPrecision.float32)
     arg_parser.add_argument("--outdir", type=str, help="Output directory", default=os.path.join("./out", datetime.now().strftime('%y.%m.%d-%H:%M:%S')+"-amortize"))
 
     cmd_args = arg_parser.parse_args()
@@ -89,7 +94,7 @@ if __name__ == "__main__":
     # Optimize mean only
     temperature = np.concatenate([np.linspace(cmd_args.t_start, cmd_args.t_end, cmd_args.epochs_mean)])
     model.objective_type = Objective.MEANKLDIVERGENCE
-    optimizer = torch.optim.Adam(model.parameters(), lr=cmd_args.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=cmd_args.lr_mean)
     loss = []
     epochs_done = 0
     for epoch in range(temperature.size):
@@ -103,7 +108,7 @@ if __name__ == "__main__":
     # Optimize full model (mean and covariance parameters)
     temperature = np.concatenate([np.linspace(cmd_args.t_start, cmd_args.t_end, cmd_args.epochs_full)])
     model.objective_type = Objective.KLDIVERGENCE
-    optimizer = torch.optim.Adam(model.parameters(), lr=cmd_args.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=cmd_args.lr_full)
     loss = []
     epochs_done = cmd_args.epochs_mean
     for epoch in range(temperature.size):
@@ -114,3 +119,7 @@ if __name__ == "__main__":
 
     torch.save(model, os.path.join(log_path, "trained_sampler.pt"))
     
+    
+    #from torch.profiler import profile, record_function, ProfilerActivity
+    #with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], with_stack=True, experimental_config=torch._C._profiler._ExperimentalConfig(verbose=True)) as prof:
+    #print(prof.key_averages(group_by_stack_n=5).table(sort_by="self_cuda_time_total", row_limit=30, max_name_column_width=200))

examples/amortization/infer.py
@@ -54,26 +54,28 @@ if __name__ == "__main__":
     arg_parser.add_argument("--sampler", type=str, help="Posterior sampler file, *.pt", default=None)
     arg_parser.add_argument("--Xfile", type=str, help="X dataset file, HDF5", default=None)
     arg_parser.add_argument("--epochs", type=int, help="Number of epochs to run the sampler", default=10)
-    arg_parser.add_argument("--batch", type=int, help="Batch size", default=32)
+    arg_parser.add_argument("--batch", type=int, help="Batch size", default=128)
     arg_parser.add_argument("--N_samples", type=int, help="Number of samples to draw", default=100)
     arg_parser.add_argument("--CPU", action="store_true")
-    arg_parser.add_argument("--precision", type=FloatPrecision, help="Compute precision", default=FloatPrecision.float64)
+    arg_parser.add_argument("--precision", type=FloatPrecision, help="Compute precision", default=FloatPrecision.float32)
     arg_parser.add_argument("--outdir", type=str, help="Output directory", default=os.path.join("./out", datetime.now().strftime('%y.%m.%d-%H:%M:%S')+"-infer"))
 
     cmd_args = arg_parser.parse_args()
+    log_path = cmd_args.outdir
+    os.makedirs(log_path, exist_ok=True)
+    sys.stdout = stdout_logger(os.path.join(log_path, "terminal.txt"))
     print("Parameters:")
     for var in vars(cmd_args):
         print("  {}: {}". format(var, vars(cmd_args)[var]))
 
+    assert cmd_args.model is not None
+    assert cmd_args.sampler is not None
+    assert cmd_args.Xfile is not None
+    
     eval("torch.set_default_dtype(torch.{})".format(cmd_args.precision))
     device = torch.device("cuda" if torch.cuda.is_available() and not cmd_args.CPU else "cpu") 
-    #torch.set_default_device(device)
     print("Using PyTorch device/precision: {}/{}".format(device, torch.get_default_dtype()))
     tvo._set_device(device)
-
-    log_path = cmd_args.outdir
-    os.makedirs(log_path, exist_ok=True)
-    sys.stdout = stdout_logger(os.path.join(log_path, "terminal.txt"))
 
     # Load BSC model parameters
     theta = load_group_as_dict(cmd_args.model, "theta")
@@ -82,7 +84,6 @@ if __name__ == "__main__":
     exp_config_dict = load_group_as_dict(cmd_args.model, "exp_config")
 
     # Load posterior sampler object from a *.pt file
-    assert cmd_args.sampler is not None
     sampler = torch.load(cmd_args.sampler, map_location=device)
     assert isinstance(sampler, SamplerModule)
     sampler.eval()

examples/amortization/models/amortizedbernoulli.py
@@ -205,7 +205,11 @@ class AmortizedBernoulli(SamplerModule):
             q_distribution = RandomFlowSequence(
                 source=UniformSource(D=H, device=device), 
                 sequence=[
-                    CollapsedBernoulliLogisticRelaxation(mu=mu, t=self.temperature), 
+                    InverseLogisticTransform(),
+                    ElementwiseLinearTransform(b=mu),
+                    AnnealingTransform(t=self.temperature),
+                    LogisticTransform(),
+                    #CollapsedBernoulliLogisticRelaxation(mu=mu, t=self.temperature), 
                     ])
             
             q_s = stable_logistic(mu)
@@ -226,7 +230,11 @@ class AmortizedBernoulli(SamplerModule):
                                             Kset=Kset, 
                                             device=device), 
                 sequence=[
-                    CollapsedBernoulliLogisticRelaxation(mu=relaxed_mu, t=self.temperature), 
+                    InverseLogisticTransform(),
+                    ElementwiseLinearTransform(b=relaxed_mu),
+                    AnnealingTransform(t=self.temperature),
+                    LogisticTransform(),
+                    #CollapsedBernoulliLogisticRelaxation(mu=relaxed_mu, t=self.temperature), 
                     ]).sample(size=(self.nsamples,))
             
             # Compute logQ of amortized posterior samples
@@ -236,7 +244,11 @@ class AmortizedBernoulli(SamplerModule):
                 sequence=[
                     LTLinearTransform(LT=L), 
                     CopulaTransform(torch.diagonal(Sigma, dim1=-1, dim2=-2)), 
-                    CollapsedBernoulliLogisticRelaxation(mu=mu, t=self.temperature), 
+                    InverseLogisticTransform(),
+                    ElementwiseLinearTransform(b=mu),
+                    AnnealingTransform(t=self.temperature),
+                    LogisticTransform(),
+                    #CollapsedBernoulliLogisticRelaxation(mu=mu, t=self.temperature), 
                     ])
             reflow_samples, log_q_s_samples = q_distribution.log_p(relaxed_Kset_samples.permute(0, 2, 1, 3))
             log_q_s_samples = log_q_s_samples.permute(0, 2, 1)
@@ -244,8 +256,8 @@ class AmortizedBernoulli(SamplerModule):
             # Compute cross-entropy        
             r_s = (marginal_p.unsqueeze(-2) * Kset + (1-marginal_p.unsqueeze(-2)) * (1-Kset)).prod(-1)  # prob. of Kset under factorized marginal distribution
             p_s = compute_probabilities(log_f)  # prob. of Kset
-            q_s = r_s / self.nsamples * (torch.exp(log_q_s_samples - relaxed_log_p).sum(0))
-            crossH_pq = -(p_s * torch.log(q_s)).sum(-1)
+            q_s = r_s  * torch.exp(log_q_s_samples - relaxed_log_p).mean(0)
+            crossH_pq = -(p_s * torch.log(torch.clamp(q_s, EPS, None))).sum(-1)
             
             H_p = stable_entropy(log_f)
             KL_pq = crossH_pq - H_p
@@ -303,7 +315,11 @@ class AmortizedBernoulli(SamplerModule):
             sequence=[
                 LTLinearTransform(LT=L), 
                 CopulaTransform(torch.diagonal(Sigma, dim1=-1, dim2=-2)), 
-                CollapsedBernoulliLogisticRelaxation(mu=mu, t=self.temperature), 
+                InverseLogisticTransform(),
+                ElementwiseLinearTransform(b=mu),
+                AnnealingTransform(t=self.temperature),
+                LogisticTransform(),
+                #CollapsedBernoulliLogisticRelaxation(mu=mu, t=self.temperature), 
                 ])
         
         q_samples, q_samples_log_p = q_distribution.sample(size=(nsamples, 1))

examples/amortization/models/common.py
@@ -196,7 +196,7 @@ class ElementwiseLinearTransform(FlowTransform):
         return fx, lp - logdet
 
     def inverse(self, fx):
-        return (fx-self.b) / self.a
+        return (fx-self.b) * (1 / self.a)
 
 
 class ScaleTransform(FlowTransform):
@@ -227,21 +227,31 @@ class AnnealingTransform(ScaleTransform):
         return 1 / self.scale
 
 
+def _stable_logistic(x):
+    return 1 / (1 + torch.exp(-x))
+    fx = torch.where(x>0, 1 / (1 + torch.exp(-x)),
+                          torch.exp(x) / (1 + torch.exp(x)))
+    return fx
+
+
+def _stable_inv_logistic(x):
+    x = torch.clamp(x, EPS, 1-EPS)
+    fx = torch.log(x/(1-x))
+    return fx
+
+
 class LogisticTransform(FlowTransform):
     def __init__(self) -> None:
         super().__init__()
         
     def forward(self, x, lp):
-        fx = 1 / (torch.exp(-x) + 1)
+        fx = _stable_logistic(x)
         logdet = (-x - 2*torch.log(torch.exp(-x) + 1)).sum(-1)  
         # logdet = (-x + 2*torch.log(fx)).sum(-1)
         return fx, lp - logdet
     
     def inverse(self, fx):
-        sigma = 1e-9
-        fx = fx*(1-sigma) + (1-fx)*sigma  # makes the function stable
-        x = torch.log(fx/(1-fx))
-        return x
+        return _stable_inv_logistic(fx)
 
 
 class InverseLogisticTransform(FlowTransform):
@@ -249,15 +259,12 @@ class InverseLogisticTransform(FlowTransform):
         super().__init__()
         
     def forward(self, x, lp):
-        sigma = 1e-9
-        x = x*(1-sigma) + (1-x)*sigma  # makes the function stable
-        fx = torch.log(x/(1-x))
-        logdet = -torch.log(x-x**2).sum(-1)
+        fx = _stable_inv_logistic(x)
+        logdet = -torch.log(torch.clamp(x-x**2, EPS, 1-EPS)).sum(-1)
         return fx, lp - logdet
     
     def inverse(self, fx):
-        x = 1 / (torch.exp(-fx) + 1)
-        return x
+        return _stable_logistic(fx)
 
 
 class CollapsedBernoulliLogisticRelaxation(FlowTransform):
@@ -281,7 +288,7 @@ class CollapsedBernoulliLogisticRelaxation(FlowTransform):
     
     def inverse(self, fx):
         fx = torch.clamp(fx, EPS, 1-EPS)
-        x = 1 / ((1 / fx - 1)**self.t / torch.exp(-self.mu) + 1)
+        x = 1 / ((1 / fx - 1)**self.t * torch.exp(self.mu) + 1)
         x = torch.clamp(x, EPS, 1-EPS)
         return x
 
@@ -316,3 +323,12 @@ if __name__ == "__main__":
     print("log-prob error: {}".format(err))
     assert err < 1e-3
     
+if __name__ == "__main__":
+    x = torch.tensor([-EPS, 0, EPS, 0.5, 1-EPS, 1, 1+EPS], dtype=torch.float32)
+    print(x)
+    print(torch.log(x))
+    inv_logistic_x = _stable_inv_logistic(x)
+    print(inv_logistic_x)
+    print(1e30*inv_logistic_x)
+    x_star = _stable_logistic(1e30*inv_logistic_x)
+    print(x_star)