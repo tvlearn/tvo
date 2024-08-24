778df82f120219ce22ddf3a1c3c56ef24c1e21da
Dmytro Velychko <dmytro.velychko@gmail.com>
2024-08-24 20:04:57 +0200

    Update slurm script

--- examples/amortization/slurm-job.sh
+++ examples/amortization/slurm-job.sh
@@ -26,11 +26,24 @@ python amortize.py \
   --Xfile ${TVODATADIR}/image_patches.h5 \
   --Ksetfile ${TVODATADIR}/training.h5 \
   --precision float32 \
-  --epochs_mean 100 \
-  --epochs_full 100  \
+  --epochs_mean 200 \
+  --epochs_full 200  \
   --batch_size 32 \
   --N_IS 32 \
   --t_start 2.0 \
   --t_end 2.0 \
-  --lr_mean 0.01 \
+  --lr_mean 0.001 \
   --lr_full 0.001
+
+
+export TVODATADIR=$(ls -d ../gaussiandenoising/out/* | tail -n 1)
+export SAMPLERDIR=$(ls -d ./out/*-amortize | tail -n 1)
+
+python infer.py \
+  --model ${TVODATADIR}/training.h5 \
+  --Xfile ${TVODATADIR}/image_patches.h5 \
+  --sampler ${SAMPLERDIR}/trained_sampler.pt \
+  --precision float32 \
+  --epochs 10 \
+  --batch 1000 \
+  --N_samples 100
