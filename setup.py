957bfb4ae0e13505f3ee27f0e67fa53b940296b7
Dmytro Velychko <dmytro.velychko@gmail.com>
2024-09-25 12:34:40 +0200

    Removed compiled module, set version 0.2.0

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
 
 setup(
     name="tvo",
+    version="0.2.0"
     packages=find_packages(exclude=("test",)),
     zip_safe=False,
-    ext_modules=[
-        Extension(
-            name="tvo.variational._set_redundant_lpj_to_low_CPU",
-            sources=["tvo/variational/_set_redundant_lpj_to_low_CPU.pyx"],
-        )
-    ],
 )
