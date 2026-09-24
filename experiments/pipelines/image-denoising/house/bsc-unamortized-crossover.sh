6f72522944efa17519e8eb21774427ec0de7a28d
Dmytro Velychko <dmytro.velychko@gmail.com>
2026-09-24 15:46:23 +0200

    Added evo runs with crossover

---
+++ experiments/pipelines/image-denoising/house/bsc-unamortized-crossover.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "$(date +'%F %T')    Executing job commands"
+
+# Commands to execute
+cd ./examples/gaussian-denoising
+env HDF5_USE_FILE_LOCKING='FALSE' python main.py bsc \
+--clean_image ./img/house.png \
+--output_directory $OUT_PATH \
+--no_epochs 4001 \
+--viz_every 100 \
+--batch_size 64 \
+--rescale 1 \
+--Ksize 200 \
+--patch_height 8 \
+-H 256 \
+--no_parents 10 \
+--no_children 9 \
+--no_generations 4 \
+--noise_level 25 \
+--crossover
+
+# End of commands to execute
